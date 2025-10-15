33079f4260ed67c203293db6c4b60770f618e1e3
DavidJohannesS <davidjohannesSD@gmail.com>
2025-10-15 17:33:36 +0200

    Adding backend

--- start.sh
+++ start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-cmd="ansible-playbook start.yaml --tags"
+cmd="ansible-playbook start.yaml --ask-become-pass --tags"
 
 options=(
     "Setup LocalDev"
     "Remove LocalDev"
+    "Setup Paparless"
+    "Remove Paperless"
     "Quit"
 )
 select opt in "${options[@]}";do
     case $REPLY in
         1) $cmd setup;;
         2) $cmd remove;;
-        10) echo "Exiting..." && break ;;
+        3) $cmd paperless;;
+        4) $cmd remove_paperless;;
+        5) echo "Exiting..." && break ;;
         *) echo "Invalid" ;;
     esac
 done
